3fc849662bf008ff41a763cc10602a36cbe54cb6
Jose Leonardo Quiñones <desarrollojlq@outlook.es>
2023-09-07 16:05:53 -0500

    Agregar archivos de proyecto.

---
+++ Licores/Models/BodegaDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Licores.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+public class BodegaDbContext : DbContext
+{
+    public BodegaDbContext(DbContextOptions<BodegaDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<ProductoModel> Productos { get; set; }
+    //public DbSet<PedidoModel> Pedidos { get; set; }
+    //public DbSet<UsuarioModel> Usuarios { get; set; }
+    //public DbSet<ProveedorModel> Proveedores { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseInMemoryDatabase("InMemoryDatabaseName"); // Nombre de la base de datos en memoria
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Aquí puedes configurar relaciones y restricciones si es necesario.
+        //// Por ejemplo, configurar la relación entre PedidoModel y ProveedorModel.
+        //modelBuilder.Entity<PedidoModel>()
+        //    .HasOne(p => p.Proveedor)
+        //    .WithMany(proveedor => proveedor.Pedidos)
+        //    .HasForeignKey(p => p.ProveedorId)
+        //    .OnDelete(DeleteBehavior.Restrict); // Puedes configurar la acción de eliminación según tus necesidades.
+    }
+}
---
+++ Licores/Models/Producto.cs
@@ -0,0 +1,15 @@
+﻿namespace Licores.Models
+{
+    public class ProductoModel
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+        public DateTime FechaVencimiento { get; set; }
+        //public TipoLicor Tipo { get; set; }
+        // Otras propiedades comunes para todos los productos
+
+        // Relación con la tabla de inventario
+        //public ICollection<InventarioModel> Inventario { get; set; }
+    }
+}
